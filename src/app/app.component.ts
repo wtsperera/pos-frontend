3592187475e99144929e4bbb8fc13c07c4c7e1ed
Thakshila1998 <wtsperera5@gmail.com>
2021-02-25 04:02:30 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html',git push -u origin maingit push -u origin main
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
